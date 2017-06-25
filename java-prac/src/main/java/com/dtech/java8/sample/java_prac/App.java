5df478be45e04d8e12eeb283b6bcbafcecdb0ed4
jitensoni24 <jitendrasoni24@gmail.com>
2017-06-25 22:28:57 +0100

    first commit

---
+++ java-prac/src/main/java/com/dtech/java8/sample/java_prac/App.java
@@ -0,0 +1,13 @@
+package com.dtech.java8.sample.java_prac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
