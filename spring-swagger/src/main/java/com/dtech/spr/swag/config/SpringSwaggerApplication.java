580d3a2515383f9bb2c30e65b0ddbaecdb8e833a
Jit-hm <jitendrasoni24@gmail.com>
2017-11-26 22:56:56 +0000

    init swagger

--- spring-swagger/src/main/java/com/dtech/spr/swag/config/SpringSwaggerApplication.java
+++ spring-swagger/src/main/java/com/dtech/spr/swag/config/SpringSwaggerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -20,4 +21,14 @@ public class SpringSwaggerApplication extends WebMvcConfigurationSupport {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new MappingJackson2HttpMessageConverter());
     }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+          .addResourceLocations("classpath:/META-INF/resources/");
+     
+        registry.addResourceHandler("/webjars/**")
+          .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    
 }
